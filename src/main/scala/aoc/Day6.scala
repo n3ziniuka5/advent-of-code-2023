44e1ece3416c2d3fd2263ee954d354a492a587e1
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2023-12-06 09:42:45 +0200

    Day 6

---
+++ src/main/scala/aoc/Day6.scala
@@ -0,0 +1,32 @@
+package aoc
+
+import aoc.Common.timed
+
+import scala.io.Source
+
+object Day6:
+    def main(args: Array[String]): Unit =
+        val lines = Source.fromResource("day6.txt").getLines().toList
+        timed("Part 1", part1(lines))
+        timed("Part 2", part2(lines))
+
+    def waysToWin(time: Long, distance: Long): Long =
+        val start = (1L to time).find { t =>
+            t * (time - t) > distance
+        }
+        time - start.get * 2 + 1
+
+    def part1(lines: List[String]): Long =
+        val time        = lines.head.drop("Time:".length).trim.split(" ").flatMap(_.trim.toIntOption).toList
+        val distance    = lines(1).drop("Distance:".length).trim.split(" ").flatMap(_.trim.toIntOption).toList
+        val timeAndDist = time.zip(distance)
+
+        timeAndDist.map { case (time, dist) =>
+            waysToWin(time, dist)
+        }.product
+
+    def part2(lines: List[String]): Long =
+        val time     = lines.head.drop("Time:".length).replace(" ", "").toLong
+        val distance = lines(1).drop("Distance:".length).replace(" ", "").toLong
+
+        waysToWin(time, distance)
