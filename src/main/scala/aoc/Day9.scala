5d8f47e7989cc8c3509c5ecf6c0031e163731def
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2023-12-09 07:24:08 +0200

    Day 9 refactor

--- src/main/scala/aoc/Day9.scala
+++ src/main/scala/aoc/Day9.scala
@@ -10,22 +10,23 @@ object Day9:
         timed("Part 1", part1(lines))
         timed("Part 2", part2(lines))
 
+    def parse(lines: List[String]): List[Vector[Long]] =
+        lines.map(_.split(" ").map(_.toLong).toVector)
+
+    extension (seq: Vector[Long])
+        def diffs: Vector[Long] =
+            seq.sliding(2, 1).map { a => a(1) - a(0) }.toVector
+
     def part1(lines: List[String]): Long =
         def solve(seq: Vector[Long]): Long =
             if (seq.forall(_ == 0)) 0
-            else {
-                val diffs = seq.sliding(2, 1).map { a => a(1) - a(0) }.toVector
-                seq.last + solve(diffs)
-            }
+            else seq.last + solve(seq.diffs)
 
-        lines.map(_.split(" ").map(_.toLong).toVector).map(solve).sum
+        parse(lines).map(solve).sum
 
     def part2(lines: List[String]): Long =
         def solve(seq: Vector[Long]): Long =
             if (seq.forall(_ == 0)) 0
-            else {
-                val diffs = seq.sliding(2, 1).map { a => a(1) - a(0) }.toVector
-                seq.head - solve(diffs)
-            }
+            else seq.head - solve(seq.diffs)
 
-        lines.map(_.split(" ").map(_.toLong).toVector).map(solve).sum
+        parse(lines).map(solve).sum
