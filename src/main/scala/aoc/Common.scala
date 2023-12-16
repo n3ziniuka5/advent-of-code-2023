717317f54e9881414a59cb7c9109a1779d7cdba9
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2023-12-16 15:49:53 +0200

    Day 16 refactor

--- src/main/scala/aoc/Common.scala
+++ src/main/scala/aoc/Common.scala
@@ -18,3 +18,41 @@ object Common:
             }
 
             q
+
+case class Point(x: Int, y: Int):
+    def up: Point    = Point(x, y - 1)
+    def down: Point  = Point(x, y + 1)
+    def left: Point  = Point(x - 1, y)
+    def right: Point = Point(x + 1, y)
+
+    def topLeft: Point     = Point(x - 1, y - 1)
+    def topRight: Point    = Point(x + 1, y - 1)
+    def bottomLeft: Point  = Point(x - 1, y + 1)
+    def bottomRight: Point = Point(x + 1, y + 1)
+
+    def adjacent = List(up, down, left, right)
+    def adjacentDiagonal = for {
+        x <- List(-1, 0, 1)
+        y <- List(-1, 0, 1) if x != 0 || y != 0
+    } yield Point(this.x + x, this.y + y)
+
+    def inBounds(map: Map2d[_]): Boolean =
+        x >= 0 && x <= map.maxX && y >= 0 && y <= map.maxY
+
+case class Map2d[V](underlying: Map[Point, V]):
+    lazy val maxX = underlying.keys.map(_.x).max
+    lazy val maxY = underlying.keys.map(_.y).max
+
+    def map[V2](f: ((Point, V)) => (Point, V2)): Map2d[V2] = Map2d(underlying.map(f))
+
+    def apply(k: Point): V = underlying(k)
+
+object Map2d:
+    def fromLines(lines: List[String]): Map2d[Char] =
+        val underlying = lines.zipWithIndex.flatMap { (line, y) =>
+            line.zipWithIndex.map { (char, x) =>
+                Point(x, y) -> char
+            }
+        }.toMap
+
+        Map2d(underlying)
--- src/main/scala/aoc/Day16.scala
+++ src/main/scala/aoc/Day16.scala
@@ -3,6 +3,7 @@ package aoc
 import aoc.Common.timed
 
 import scala.io.Source
+import scala.collection.parallel.CollectionConverters._
 
 object Day16:
     def main(args: Array[String]): Unit =
@@ -11,67 +12,47 @@ object Day16:
         timed("Part 2", part2(lines))
 
     def part1(lines: List[String]): Long =
-        val points = parse(lines)
-        val maxX   = points.keys.map(_.x).max
-        val maxY   = points.keys.map(_.y).max
-        energized(List((Point(0, 0), Point(-1, 0))), Set.empty, maxX, maxY, points)
+        val map = Map2d.fromLines(lines)
+        energized(List((Point(0, 0), Point(-1, 0))), Set.empty, map)
 
     def part2(lines: List[String]): Long =
-        val points = parse(lines)
-        val maxX   = points.keys.map(_.x).max
-        val maxY   = points.keys.map(_.y).max
+        val map = Map2d.fromLines(lines)
 
-        val downwards = (0 to maxX).map { x =>
+        val downwards = (0 to map.maxX).map { x =>
             val point = Point(x, 0)
             List((point, point.up))
         }
-        val upwards = (0 to maxX).map { x =>
-            val point = Point(x, maxY)
+        val upwards = (0 to map.maxX).map { x =>
+            val point = Point(x, map.maxY)
             List((point, point.down))
         }
 
-        val fromLeft = (0 to maxY).map { y =>
+        val fromLeft = (0 to map.maxY).map { y =>
             val point = Point(0, y)
             List((point, point.left))
         }
 
-        val fromRight = (0 to maxY).map { y =>
-            val point = Point(maxX, y)
+        val fromRight = (0 to map.maxY).map { y =>
+            val point = Point(map.maxX, y)
             List((point, point.right))
         }
 
         val allStarts = downwards ++ upwards ++ fromLeft ++ fromRight
-        allStarts.map { start =>
-            energized(start, Set.empty, maxX, maxY, points)
+        allStarts.par.map { start =>
+            energized(start, Set.empty, map)
         }.max
-
-    case class Point(x: Int, y: Int):
-        def up: Point    = Point(x, y - 1)
-        def down: Point  = Point(x, y + 1)
-        def left: Point  = Point(x - 1, y)
-        def right: Point = Point(x + 1, y)
-        def inBounds(maxX: Int, maxY: Int): Boolean =
-            x >= 0 && x <= maxX && y >= 0 && y <= maxY
-
-    def parse(lines: List[String]): Map[Point, Char] =
-        lines.zipWithIndex.flatMap { (line, y) =>
-            line.zipWithIndex.map { (c, x) =>
-                Point(x, y) -> c
-            }
-        }.toMap
 
     def energized(
         search: List[(Point, Point)],
         visited: Set[(Point, Point)],
-        maxX: Int,
-        maxY: Int,
-        points: Map[Point, Char]
+        map: Map2d[Char]
     ): Int =
         if (search.isEmpty) visited.map(_._1).size
-        else if (visited.contains(search.head)) energized(search.tail, visited, maxX, maxY, points)
+        else if (visited.contains(search.head) || visited.contains(search.head.swap))
+            energized(search.tail, visited, map)
         else
             val (current, previous) = search.head
-            val newSearches = points(current) match
+            val newSearches = map(current) match
                 case '.' =>
                     if (previous == current.up) List((current.down, current))
                     else if (previous == current.down) List((current.up, current))
@@ -99,9 +80,7 @@ object Day16:
                     else List((current.down, current))
 
             energized(
-              newSearches.filterNot(visited.contains).filter(_._1.inBounds(maxX, maxY)) ++ search.tail,
+              newSearches.filter(_._1.inBounds(map)) ++ search.tail,
               visited + search.head,
-              maxX,
-              maxY,
-              points
+              map
             )
